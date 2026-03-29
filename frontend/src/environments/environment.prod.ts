8b29beaf495361857fa5b202194660cdc8053c7e
Trynos Zhou <trynoszhou2010@gmail.com>
2026-03-29 07:05:06 +0200

    Fixing route

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // For production, use the actual backend API URL
-//const apiUrl = 'https://sms-pro-sec.onrender.com/api';
-const apiUrl='https://sms-pro-sec.onrender.com';
+const apiUrl = 'https://sms-pro-sec.onrender.com/api';
+//const apiUrl='https://sms-pro-sec.onrender.com';
 
 export const environment = {
   production: true,
